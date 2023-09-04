01b7cb7c1c029663f787b748b4d4ed11e6269650
AtulaRiver <ngiang1309@gmail.com>
2023-09-04 10:43:54 +0700

    refresh lesson

--- ls-7-callback/method.js
+++ ls-7-callback/method.js
@@ -1,130 +1,102 @@
 // * forEach() method callback
 
-// var courses = [
-//     'Javascript',
-//     'Ruby',
-//     'PHP'
-// ]
+var courses = [
+    'Javascript',
+    'Ruby',
+    'PHP'
+]
 
-// Array.prototype.myForeach = function (callback) {
-//     for (var index in this) {
-//         if(this.hasOwnProperty(index)) {
-//             callback(this[index], index, this);
-//         }
-//     }
-// }
+Array.prototype.myForeach = function (callback) {
+    for (var index in this) {
+        if(this.hasOwnProperty(index)) {
+            callback(this[index], index, this);
+        }
+    }
+}
 
-// console.log(courses);
+console.log(courses);
 
-// var learn = courses.myForeach((course, index, array) => {
-//     console.log(course, index, array)
-// })
+var learn = courses.myForeach((course, index, array) => {
+    console.log(course, index, array)
+})
 
 
 // * filter() method callback
 
-// Array.prototype.myFilter = function (callback) {
-//     var output = [];
-//     for (var index in this) {
-//         if (this.hasOwnProperty(index)) {
-//             var result = callback(this[index], index, this);
-//             if(result) {
-//                 output.push(this[index]);
-//             }
-//         }
-//     }
-//     return output;
-// }
+Array.prototype.myFilter = function (callback) {
+    var output = [];
+    for (var index in this) {
+        if (this.hasOwnProperty(index)) {
+            var result = callback(this[index], index, this);
+            if(result) {
+                output.push(this[index]);
+            }
+        }
+    }
+    return output;
+}
 
-// var course = [
-//     {
-//         name: "Javascript",
-//         coin: 0,
-//     },
-//     {
-//         name: "HTML, CSS",
-//         coin: 680,
-//     },
-//     {
-//         name: "Ruby",
-//         coin: 860,
-//     },
-//     {
-//         name: "Ruby",
-//         coin: 720,
-//     }
-// ]
+var course = [
+    {
+        name: "Javascript",
+        coin: 0,
+    },
+    {
+        name: "HTML, CSS",
+        coin: 680,
+    },
+    {
+        name: "Ruby",
+        coin: 860,
+    },
+    {
+        name: "Ruby",
+        coin: 720,
+    }
+]
 
-// var filterCourses = course.myFilter((course, index, array) => {
-//     return course.coin > 700;
-// })
+var filterCourses = course.myFilter((course, index, array) => {
+    return course.coin > 700;
+})
 
-// console.log(filterCourses);
+console.log(filterCourses);
 
 // * mySome() method callback
 
-// var courses = [
-//     {
-//         name: 'Javascript',
-//         coin: 680,
-//         isFinish: false,
-//     },
-//     {
-//         name: 'PHP',
-//         coin: 860,
-//         isFinish: true,
-//     },
-//     {
-//         name: 'Ruby',
-//         coin: 860,
-//         isFinish: false,
-//     },
-// ];
+var courses = [
+    {
+        name: 'Javascript',
+        coin: 680,
+        isFinish: false,
+    },
+    {
+        name: 'PHP',
+        coin: 860,
+        isFinish: true,
+    },
+    {
+        name: 'Ruby',
+        coin: 860,
+        isFinish: false,
+    },
+];
 
-// Array.prototype.mySome = function(cb) {
-//     for(var index in courses) {
-//         if(this.hasOwnProperty(index)) {
-//             if(cb(this[index]), index, this) {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// }
-
-// console.log(courses.mySome((course, index, array) => {
-//     return course.isFinish === true;
-// }));
-
-Array.prototype.myEvery = function(cb) {
-    for(var index in this) {
+Array.prototype.mySome = function(cb) {
+    for(var index in courses) {
         if(this.hasOwnProperty(index)) {
-            if(!cb(this[index], index, this)) {
-                return false;
+            if(cb(this[index]), index, this) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
 
+console.log(courses.mySome((course, index, array) => {
+    return course.isFinish === true;
+}));
 
-const numbers = [1, 3, 3, 5];
 
-for(var i in numbers) {
-    console.log(i);
-}
-
-console.log(numbers.myEvery(function (number) {
-    return number % 2 !== 0;
-})); // Output: true
-
-console.log(numbers.myEvery(function (number, index) {
-    return index % 2 === 0;
-})); // Output: false
-
-console.log(numbers.myEvery(function (number, index, array) {
-    return array.length % 2 === 0;
-})); // Output: true
 
 
 
